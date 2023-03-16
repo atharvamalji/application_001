134bd28d8f915951816656b98a2160ff66f7e4c4
Atharva Malji <atharva.malji.2001@gmail.com>
2023-03-16 20:08:29 +0530

    initialized project and created smart contract

---
+++ README.md
@@ -0,0 +1,7 @@
+# Crowdfunding Web 3.0 Application 🌐
+
+### Application Details
+---
+### Name : crowdfundthree
+### Code Name : application_001
+---
---
+++ crowdfundthree/contracts/CrowdFunding.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract CrowdFunding {
+    struct Campaign {
+        address owner;
+        
+        string title;
+        string image;
+        string description;
+
+        uint256 target;
+        uint256 deadline;
+        uint256 amountCollected;
+
+        address[] donators;
+        uint256[] donations;
+    }
+
+    mapping (uint256 => Campaign) public campaigns;
+
+    uint256 public numberOfCampaigns = 0;
+
+    function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image ) public returns (uint256) {
+        Campaign storage campaign = campaigns[numberOfCampaigns];
+
+        require(campaign.deadline < block.timestamp, "The deadline should be a date in the future.");
+
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.image = _image;
+
+        campaign.amountCollected = 0;
+
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.description = _description;
+
+        numberOfCampaigns++;
+
+        return numberOfCampaigns - 1;
+    }
+
+    function donateToCampaign(uint256 _id) public payable {
+        uint256 amount = msg.value;
+
+        Campaign storage campaign = campaigns[_id];
+
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
+
+        (bool sent,  ) = payable(campaign.owner).call{value: amount}("");
+
+        if(sent) {
+            campaign.amountCollected = campaign.amountCollected + amount;
+        }
+    }
+
+    function getDonators(uint256 _campaignId) public view returns (address[] memory, uint256[] memory) {
+        return (campaigns[_campaignId].donators, campaigns[_campaignId].donations);
+    }
+
+    function getCampaigns() public view returns (Campaign[] memory) {
+        Campaign[] memory allCampaigns = new Campaign[] (numberOfCampaigns);
+
+        for (uint256 i = 0; i < numberOfCampaigns; i++) {
+            Campaign storage item = campaigns[i];
+            allCampaigns[i] = item;
+        }
+
+        return allCampaigns;
+    }
+}
